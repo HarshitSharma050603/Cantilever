6b0ee2329f65a4a38eaaba4bb5a84883bd952bdc
HarshitSharma050603 <harshit1063sharma@gmail.com>
2025-07-20 21:50:00 +0530

    Success

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/Authcontext"; // ✅ Adjust path if needed
+import { AuthProvider } from "./Context/Authcontext"; // ✅ Adjust path if needed
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </AuthProvider>
   </React.StrictMode>
-);
+);n
